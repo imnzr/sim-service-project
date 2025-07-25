3aa1679b1f7a1434bea4563d01f542188035c2c0
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-25 21:20:44 +0700

    create: config function

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfig struct {
+	// XenditApiKey         string
+	// XenditCallbackToken  string
+	// SimServiceAPIKey     string
+	AppPort              string
+	JWTSecretKey         string
+	AccessTokenDuration  time.Duration
+	RefreshTokenDuration time.Duration
+	DatabaseURL          string
+}
+
+func LoadConfig() *AppConfig {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("no .env file found, assuming environment variables are set: %v", err)
+	}
+
+	cfg := &AppConfig{
+		// XenditApiKey: os.Getenv("XENDIT_API_KEY"),
+		// XenditCallbackToken: os.Getenv(),
+		// SimServiceAPIKey:    os.Getenv(),
+		AppPort:      os.Getenv("APP_PORT"),
+		JWTSecretKey: os.Getenv("JWT_SECRET_KEY"),
+		DatabaseURL:  os.Getenv("DATABASE_URL"),
+	}
+
+	if cfg.DatabaseURL == "" {
+		log.Fatal("XENDIT_API_KEY environment variable not set")
+	}
+
+	return cfg
+}
