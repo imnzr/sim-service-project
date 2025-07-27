e814773ad628ad0ceced7f3936b8734bc1f17807
Nizar Fadilah <nizarfadilah74@gmail.com>
2025-07-27 22:33:32 +0700

    add: new environment

--- config/config.go
+++ config/config.go
@@ -11,7 +11,8 @@ import (
 type AppConfig struct {
 	// XenditApiKey         string
 	// XenditCallbackToken  string
-	// SimServiceAPIKey     string
+	SimServiceAPIKey     string
+	SimUrlDefault        string
 	AppPort              string
 	JWTSecretKey         string
 	AccessTokenDuration  time.Duration
@@ -28,14 +29,21 @@ func LoadConfig() *AppConfig {
 	cfg := &AppConfig{
 		// XenditApiKey: os.Getenv("XENDIT_API_KEY"),
 		// XenditCallbackToken: os.Getenv(),
-		// SimServiceAPIKey:    os.Getenv(),
-		AppPort:      os.Getenv("APP_PORT"),
-		JWTSecretKey: os.Getenv("JWT_SECRET_KEY"),
-		DatabaseURL:  os.Getenv("DATABASE_URL"),
+		SimServiceAPIKey: os.Getenv("SIM_API_KEY_SERVICE"),
+		SimUrlDefault:    os.Getenv("SIM_API_URL_SERVICE"),
+		AppPort:          os.Getenv("APP_PORT"),
+		JWTSecretKey:     os.Getenv("JWT_SECRET_KEY"),
+		DatabaseURL:      os.Getenv("DATABASE_URL"),
 	}
 
 	if cfg.DatabaseURL == "" {
 		log.Fatal("XENDIT_API_KEY environment variable not set")
+	}
+	if cfg.SimServiceAPIKey == "" {
+		log.Fatal("SIM_SERVICE_API_KEY environment variable not set")
+	}
+	if cfg.SimUrlDefault == "" {
+		log.Fatal("SIM_API_URL_SERVICE environment variable not set")
 	}
 
 	return cfg
